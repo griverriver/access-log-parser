6cdee070f889ea7ad7fb4070d3796c2e0acb1012
Elena Kravtsova <griverriver@yandex.ru>
2025-11-30 17:37:51 +0300

    Добавлены операции для двух чисел

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число:");
+        int number1 = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int number2 = new Scanner(System.in).nextInt();
+        int sum = number1 + number2;
+        int diff = number1 - number2;
+        int product = number1 * number2;
+        double quotient = (double) number1 / number2;
+        System.out.println("Сумма двух чисел = " + sum);
+        System.out.println("Разность двух чисел = " + diff);
+        System.out.println("Произведение двух чисел = " + product);
+        System.out.println("Частное двух чисел = " + quotient);
     }
 }
