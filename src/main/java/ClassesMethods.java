19219ee30e66961b5301fab6fbe9e0ff3e5c2a8a
Elena Kravtsova <griverriver@yandex.ru>
2025-12-10 14:21:08 +0300

    Исправлены отступы

--- src/main/java/ClassesMethods.java
+++ src/main/java/ClassesMethods.java
@@ -6,58 +6,58 @@ public class ClassesMethods {
         System.out.println(isPositive(-43));
         System.out.println(is2Digits(47));
         System.out.println(isUpperCase('R'));
-        System.out.println(isInRange(6,1,4));
-        System.out.println(isDivisor(4,7));
-        System.out.println(isEqual(3,3,3));
+        System.out.println(isInRange(6, 1, 4));
+        System.out.println(isDivisor(4, 7));
+        System.out.println(isEqual(3, 3, 3));
 
 
-        int numSum = lastNumSum(5,11);
-        numSum = lastNumSum(numSum,123);
-        numSum = lastNumSum(numSum,14);
-        numSum = lastNumSum(numSum,1);
+        int numSum = lastNumSum(5, 11);
+        numSum = lastNumSum(numSum, 123);
+        numSum = lastNumSum(numSum, 14);
+        numSum = lastNumSum(numSum, 1);
         System.out.println(numSum);
 
     }
 
-    public static double fraction(double x){
+    public static double fraction(double x) {
         return x - (int) x;
     }
 
-    public static int sumLastNums(int x){
-        int a = x%10;
-        int b = (x/10)%10;
+    public static int sumLastNums(int x) {
+        int a = x % 10;
+        int b = (x / 10) % 10;
         return a + b;
     }
 
-    public static int charToNum(char x){
+    public static int charToNum(char x) {
         return (int) x;
     }
 
-    public static boolean isPositive(int x){
-        return x>0;
+    public static boolean isPositive(int x) {
+        return x > 0;
     }
 
-    public static boolean is2Digits(int x){
-        return (x>9)&&(x<100);
+    public static boolean is2Digits(int x) {
+        return (x > 9) && (x < 100);
     }
 
-    public static boolean isUpperCase(char x){
+    public static boolean isUpperCase(char x) {
         return (x >= 'A') && (x <= 'Z');
     }
 
-    public static boolean isInRange(int a, int b, int num){
+    public static boolean isInRange(int a, int b, int num) {
         return ((num >= a) && (num <= b)) || ((num >= b) && (num <= a));
     }
 
-    public static boolean isDivisor (int a, int b){
+    public static boolean isDivisor(int a, int b) {
         return (a % b == 0) || (b % a == 0);
     }
 
-    public static boolean isEqual (int a, int b, int c){
+    public static boolean isEqual(int a, int b, int c) {
         return (a == b) && (b == c);
     }
 
-    public static int lastNumSum(int a, int b){
-        return (a%10)+(b%10);
+    public static int lastNumSum(int a, int b) {
+        return (a % 10) + (b % 10);
     }
 }
--- src/main/java/ConditionalStatements.java
+++ src/main/java/ConditionalStatements.java
@@ -1,87 +1,103 @@
 public class ConditionalStatements {
     public static void main(String[] args) {
         System.out.println(abs(8));
-        System.out.println(safeDiv(12,0));
-        System.out.println(max(4,8));
-        System.out.println(makeDecision(9,9));
-        System.out.println(max3(4,7,9));
-        System.out.println(sum3(2,5,7));
-        System.out.println(sum2(5,5));
+        System.out.println(safeDiv(12, 0));
+        System.out.println(max(4, 8));
+        System.out.println(makeDecision(9, 9));
+        System.out.println(max3(4, 7, 9));
+        System.out.println(sum3(2, 5, 7));
+        System.out.println(sum2(5, 5));
         System.out.println(is35(5));
-        System.out.println(magic6(1,6));
+        System.out.println(magic6(1, 6));
         System.out.println(age(24));
         System.out.println(day(5));
         printDays("среда");
     }
 
 
-    public static int abs(int x){
+    public static int abs(int x) {
         if (x < 0) return -x;
         return x;
     }
 
-    public static int safeDiv(int x, int y){
+    public static int safeDiv(int x, int y) {
         if (y == 0) return 0;
-        return x/y;
+        return x / y;
     }
 
-    public static int max(int x, int y){
+    public static int max(int x, int y) {
         if (x > y) return x;
         return y;
     }
 
-    public static String makeDecision(int x, int y){
-        if (x>y) return x + ">" + y;
-        if (x<y) return x + "<" + y;
+    public static String makeDecision(int x, int y) {
+        if (x > y) return x + ">" + y;
+        if (x < y) return x + "<" + y;
         return x + "==" + y;
     }
 
-    public static int max3(int x, int y, int z){
-        if ((x>=y)&&(x>=z)) return x;
-        if ((y>=x)&&(y>=z)) return y;
+    public static int max3(int x, int y, int z) {
+        if ((x >= y) && (x >= z)) return x;
+        if ((y >= x) && (y >= z)) return y;
         return z;
     }
 
-    public static boolean sum3(int x, int y, int z){
-        return ((x+y==z)||(y+z==x)||(x+z==y));
+    public static boolean sum3(int x, int y, int z) {
+        return ((x + y == z) || (y + z == x) || (x + z == y));
     }
 
-    public static int sum2(int x, int y){
-        if (((x+y)>=10)&&((x+y)<=19)) return 20;
-        return x+y;
+    public static int sum2(int x, int y) {
+        if (((x + y) >= 10) && ((x + y) <= 19)) return 20;
+        return x + y;
     }
 
-    public static boolean is35(int x){
-        return !(((x%3==0)&&(x%5==0))||((x%3!=0)&&(x%5!=0)));
+    public static boolean is35(int x) {
+        return !(((x % 3 == 0) && (x % 5 == 0)) || ((x % 3 != 0) && (x % 5 != 0)));
     }
 
-    public static boolean magic6(int x, int y){
-        return ((x==6)||(y==6)||(x+y==6)||(x-y==6)||(y-x==6));
+    public static boolean magic6(int x, int y) {
+        return ((x == 6) || (y == 6) || (x + y == 6) || (x - y == 6) || (y - x == 6));
     }
 
-    public static String age(int x){
-        if ((x%10==1)&&(x%100!=11)) return x + " год";
-        if (((x%10==2)&&(x%100!=12))||((x%10==3)&&(x%100!=13))||((x%10==4)&&(x%100!=14))) return x + " года";
+    public static String age(int x) {
+        if ((x % 10 == 1) && (x % 100 != 11)) return x + " год";
+        if (((x % 10 == 2) && (x % 100 != 12)) || ((x % 10 == 3) && (x % 100 != 13)) || ((x % 10 == 4) && (x % 100 != 14)))
+            return x + " года";
         return x + " лет";
     }
 
-    public static String day(int x){
+    public static String day(int x) {
         String strDay;
-        switch (x){
-            case 1: strDay = "понедельник"; break;
-            case 2: strDay = "вторник"; break;
-            case 3: strDay = "среда"; break;
-            case 4: strDay = "четверг"; break;
-            case 5: strDay = "пятница"; break;
-            case 6: strDay = "суббота"; break;
-            case 7: strDay = "воскресенье"; break;
-            default: strDay = "это не день недели";
+        switch (x) {
+            case 1:
+                strDay = "понедельник";
+                break;
+            case 2:
+                strDay = "вторник";
+                break;
+            case 3:
+                strDay = "среда";
+                break;
+            case 4:
+                strDay = "четверг";
+                break;
+            case 5:
+                strDay = "пятница";
+                break;
+            case 6:
+                strDay = "суббота";
+                break;
+            case 7:
+                strDay = "воскресенье";
+                break;
+            default:
+                strDay = "это не день недели";
         }
         return strDay;
     }
 
-    public static void printDays(String x){
-        switch (x){
+    public static void printDays(String x) {
+        switch (x) {
             case "понедельник":
                 System.out.println("понедельник");
             case "вторник":
@@ -95,7 +111,8 @@ public class ConditionalStatements {
             case "суббота":
                 System.out.println("суббота");
             case "воскресенье":
-                System.out.println("воскресенье"); break;
+                System.out.println("воскресенье");
+                break;
             default:
                 System.out.println("это не день недели");
         }
