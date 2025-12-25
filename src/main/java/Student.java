f2d09c84ca6db299b3abb2f3b242fd4b36373951
Elena Kravtsova <griverriver@yandex.ru>
2025-12-25 17:24:29 +0300

    Практика ООП. Инкапсуляция #3. Студент

---
+++ src/main/java/Student.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Student {
+    private final String name;
+    private final ArrayList<Integer> grades;
+
+    Student(String name, ArrayList<Integer> grades) {
+        this.name = name;
+        this.grades = new ArrayList<>();
+        if (grades != null) {
+            for (int grade : grades)
+                if (grade >= 2 && grade <= 5) {
+                    this.grades.add(grade);
+                }
+        }
+    }
+
+    Student(String name) {
+        this(name, new ArrayList<>());
+    }
+
+    public String toString() {
+        return name + ": " + grades;
+    }
+
+    public void addGrade(int grade) {
+        if (grade >= 2 && grade <= 5) {
+            this.grades.add(grade);
+        } else {
+            throw new IllegalArgumentException("Оценка должна быть в диапазоне от 2 до 5");
+        }
+    }
+
+    public ArrayList<Integer> getGrades() {
+        return new ArrayList<>(this.grades);
+    }
+}
