82884a1dbd78bf12a92d2cbadaf1db8e600859e5
Elena Kravtsova <griverriver@yandex.ru>
2026-01-15 19:10:32 +0300

    Практика ООП. Полиморфизм #1. Трехмерная точка

---
+++ src/main/java/Point1.java
@@ -0,0 +1,33 @@
+class Point1 {
+    int x, y;
+
+    public Point1(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    void coordinates() {
+        System.out.println(this);
+    }
+
+    public String toString() {
+        return "{" + x + ";" + y + '}';
+    }
+}
+
+class Point3D extends Point1 {
+    int z;
+    public Point3D(int x, int y, int z) {
+        super(x, y);
+        this.z=z;
+
+    }
+
+    public void coordinates() {
+        System.out.println(this);
+    }
+
+    public String toString() {
+        return "{" + x + ";" + y + ";" + z + '}';
+    }
+}
