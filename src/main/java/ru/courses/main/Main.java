68389b787ad1083377b93c450bdbdccc392938f9
Elena Kravtsova <griverriver@yandex.ru>
2026-01-19 14:15:34 +0300

    Практика ООП. Пакеты #4. Простые имена

--- src/main/java/ru/courses/main/Main.java
+++ src/main/java/ru/courses/main/Main.java
@@ -1,10 +1,18 @@
 package ru.courses.main;
 
+import ru.courses.math.geometry.Point1;
+
+import java.awt.*;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите первое число:");
+        Point1 point1 = new Point1(3, 7);
+        java.awt.Point point2 = new Point(8, 9);
+        System.out.println(point1);
+        System.out.println(point2);
+
+        /*System.out.println("Введите первое число:");
         int number1 = new Scanner(System.in).nextInt();
         System.out.println("Введите второе число:");
         int number2 = new Scanner(System.in).nextInt();
@@ -15,6 +23,6 @@ public class Main {
         System.out.println("Сумма двух чисел = " + sum);
         System.out.println("Разность двух чисел = " + diff);
         System.out.println("Произведение двух чисел = " + product);
-        System.out.println("Частное двух чисел = " + quotient);
+        System.out.println("Частное двух чисел = " + quotient);*/
     }
 }
