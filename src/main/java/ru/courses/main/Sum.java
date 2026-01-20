11e9f3c82700be2de7c0056e8757abb6a5b4877b
Elena Kravtsova <griverriver@yandex.ru>
2026-01-20 19:45:46 +0300

    Практика ООП. Исключения #2. Необрабатываемые исключения

--- src/main/java/ru/courses/main/Sum.java
+++ src/main/java/ru/courses/main/Sum.java
@@ -4,9 +4,9 @@ import ru.courses.basics.Array;
 
 public class Sum {
     public static void main(String[] args) {
-        String[] strArray = {"3","8","dkjfh84","4"};
+        String[] strArray = {"3.6","8","dkjfh84","4"};
         double sum = 0;
-        for (String s : strArray) {
+        /*for (String s : strArray) {
             try {
                 sum += Double.parseDouble(s);
             }
@@ -14,7 +14,12 @@ public class Sum {
                 ex = new NumberFormatException("Это не число: " + s);
                 System.out.println(ex);
             }
+        }*/
 
+        for (String s : strArray) {
+            if (s.matches("-?\\d+(\\.\\d+)?")) {
+                sum += Double.parseDouble(s);
+            }
         }
         System.out.println(sum);
     }
